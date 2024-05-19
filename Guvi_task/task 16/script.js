0db38025ee57659604ee77df822c360fe9f761fb
goku-code <gedu409@gmail.com>
2024-05-19 16:00:17 +0530

    create

---
+++ Guvi_task/task 16/script.js
@@ -0,0 +1,30 @@
+setTimeout(function(){
+    document.getElementById("countdown").textContent="9";
+    setTimeout(function(){
+        document.getElementById("countdown").textContent="8";
+        setTimeout(function(){
+            document.getElementById("countdown").textContent="7";
+            setTimeout(function(){
+                document.getElementById("countdown").textContent="6";
+                setTimeout(function(){
+                    document.getElementById("countdown").textContent="5";
+                    setTimeout(function(){
+                        document.getElementById("countdown").textContent="4";
+                        setTimeout(function(){
+                            document.getElementById("countdown").textContent="3";
+                            setTimeout(function(){
+                                document.getElementById("countdown").textContent="2";
+                                setTimeout(function(){
+                                    document.getElementById("countdown").textContent="1";
+                                    setTimeout(function(){
+                                        document.getElementById("countdown").textContent="Happy Independence Day!!!";
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000);
