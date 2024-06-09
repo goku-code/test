ca1699c0d73caed2eca27e5b7e42be776de11d9d
goku-code <70147621+goku-code@users.noreply.github.com>
2024-06-09 16:53:13 +0530

    Update script.js

--- Guvi_task/task 16/script.js
+++ Guvi_task/task 16/script.js
@@ -1,30 +1,63 @@
-setTimeout(function(){
-    document.getElementById("countdown").textContent="9";
-    setTimeout(function(){
-        document.getElementById("countdown").textContent="8";
-        setTimeout(function(){
-            document.getElementById("countdown").textContent="7";
-            setTimeout(function(){
-                document.getElementById("countdown").textContent="6";
-                setTimeout(function(){
-                    document.getElementById("countdown").textContent="5";
-                    setTimeout(function(){
-                        document.getElementById("countdown").textContent="4";
-                        setTimeout(function(){
-                            document.getElementById("countdown").textContent="3";
-                            setTimeout(function(){
-                                document.getElementById("countdown").textContent="2";
-                                setTimeout(function(){
-                                    document.getElementById("countdown").textContent="1";
-                                    setTimeout(function(){
-                                        document.getElementById("countdown").textContent="Happy Independence Day!!!";
-                                    },1000);
-                                },1000);
-                            },1000);
-                        },1000);
-                    },1000);
-                },1000);
-            },1000);
-        },1000);
-    },1000);
-},1000);
+// function sum(a,b){
+//     return a+b;
+// }
+// function display(sum){
+//     console.log("sum is ",sum);
+// }
+// function add(a,b,sum,display){
+//     return display(sum(a,b))
+// }
+
+// add(5,20,sum,display);
+
+// a=[10,4,65,32,3]
+
+// function forEach(a,callback){
+//     for(let i=0;i<a.length;i++){
+//         callback(a[i],i)
+//     }
+// }
+
+// forEach(a,(e,index)=>{
+//     console.log(index,e);
+// })
+
+var div=document.getElementById("root")
+setTimeout(()=>{
+
+div.innerText="10";
+setTimeout(()=>{
+
+    div.innerText="9";
+setTimeout(()=>{
+
+    div.innerText="8";
+    setTimeout(()=>{
+ 
+        div.innerText="7";
+        setTimeout(()=>{
+
+            div.innerText="6";
+            setTimeout(()=>{
+                div.innerText="5";
+                setTimeout(()=>{
+                    div.innerText="4";
+                setTimeout(()=>{
+                    div.innerText="3";
+                    setTimeout(()=>{
+                        div.innerText="2";
+                        setTimeout(()=>{
+                            div.innerText="1";
+                            setTimeout(()=>{
+                                div.innerHTML="<h2><b>Happy Independence Day!!</h2></b>";
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+    },1000)
+},1000)
